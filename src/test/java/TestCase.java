f4ba6625b72151cca1b09f56af91fc963b7cc11d
Niharika <neeharikakonduru@gmail.com>
2021-07-12 22:13:12 +0530

    My first jenkins commit

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -22,6 +22,13 @@ public class TestCase {
 	driver.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 		driver.findElement(By.id("identifierId")).sendKeys("neeharikakonduru@gmail.com");
 		driver.findElement(By.xpath("//div[@class='zQJV3']/div/div/div/div/button/span[@jsname='V67aGc']")).click();
+		
+		
+		/*
+		 * added
+		 * new
+		 * comments
+		*/
 	
 	}
 	
